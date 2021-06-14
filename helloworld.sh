2b0f5402807ad90e09989b2eeae0601aa284255d
luyaar <52702297+luyanda123@users.noreply.github.com>
2021-06-14 13:37:43 +0200

    Create helloworld.sh

---
+++ helloworld.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+# Create a Hello World executable application, 
+# choose the code on any of the following scripting languages, 
+# include instructions to run the application.
+
+# To run the app please save the bash file and run in as shown below
+# ./helloworld.sh
+
+# Asking for the user to enter their name
+echo Please enter your name:
+read username
+echo Hello $username
+echo Thank you for your entering your name
